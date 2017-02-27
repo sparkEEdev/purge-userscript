53256966afbe333c8468e0fd3222ede1cd2d4497
Sasa Marjanovic <sparkeeftw@gmail.com>
2017-02-27 10:54:45 +0100

    add few statements to properly select buttons

--- purge.js
+++ purge.js
@@ -3,7 +3,7 @@
 // @namespace https://csgolounge.com/
 // @author Saša Marjanović <sasa.marjanovic1994@gmail.com>
 // @description Automated userscript which removes comments from CSGOLounge trades.
-// @version 2.0
+// @version 2.1
 // @include https://csgolounge.com/mytrades
 // @include http://csgolounge.com/mytrades
 // @include https://csgolounge.com/trade?t=*
@@ -15,7 +15,9 @@ var url = [
     "https://csgolounge.com/mytrades",
     "http://csgolounge.com/mytrades",
     "https://www.csgolounge.com/mytrades",
-    "http://www.csgolounge.com/mytrades"
+    "http://www.csgolounge.com/mytrades",
+    "http://csgolounge.com/img/trash.png",
+    "http://csgolounge.com/img/ban.png"
 ];
 
 function save(arr) {
@@ -41,22 +43,23 @@ if (window.location.href.match(url[0] || url[1] || url[2] || url[3])) {
         window.location.replace(init);
     });
 } else if (window.location.href.match(/https:\/\/csgolounge\.com\/trade\?t=[0-9]*#purge/)) {
-    var trashCan = document.querySelectorAll('.opts');
+    var msgOpts = document.querySelectorAll('.opts');
     var clean = document.querySelectorAll('.half')[1].children[0];
     var msg = document.querySelectorAll('.message');
     var linksArr2 = JSON.parse(sessionStorage.KEY);
     var link = linksArr2.shift();
 
     if (msg.length > 2) {
-        trashCan.forEach(function (val) {
+        msgOpts.forEach(function (val) {
             var remove = val.children[0];
             var block = val.children[1];
             if (val.parentElement.children[2].innerText.match(/http/i)) {
-                block.click();
-                remove.click();
+                if ((remove.firstChild.src == url[4]) && (block.firstChild.src == url[5])) {
+                    block.click();
+                    remove.click();
+                } else if (remove.firstChild.src == url[5]) remove.click();
             } else remove.click();
         });
-
         setTimeout(function () {
             clean.click();
         }, 1000);
